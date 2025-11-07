46de80004cdca747a3a56bc3aab5aab4057e461f
Nima <nimaginobilisalami@gmail.com>
2025-11-07 10:48:22 +0100

    toResponse() i PerformedSetMapper klasse

--- src/main/java/com/example/gymrat_backend/mapper/PerformedSetMapper.java
+++ src/main/java/com/example/gymrat_backend/mapper/PerformedSetMapper.java
@@ -1,4 +1,27 @@
 package com.example.gymrat_backend.mapper;
 
+import com.example.gymrat_backend.dto.response.PerformedSetResponse;
+import com.example.gymrat_backend.model.PerformedSet;
+
 public class PerformedSetMapper {
+
+    private PerformedSetMapper() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    // PerformedSet -> PerformedSetResponse
+    public static PerformedSetResponse toResponse(PerformedSet set) {
+        if (set == null) return null;
+
+        PerformedSetResponse response = new PerformedSetResponse();
+        response.setPerformedSetId(set.getPerformedSetId());
+        response.setSideOfBody(set.getSideOfBody());
+        response.setSetNumber(set.getSetNumber());
+        response.setWeight(set.getWeight());
+        response.setReps(set.getReps());
+        response.setDurationSeconds(set.getDurationSeconds());
+        response.setNote(set.getNote());
+
+        return response;
+    }
 }
